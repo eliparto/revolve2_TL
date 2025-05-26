f304c412c321109ace912a0408d99be224c027cc
Eli P <ermparto@gmail.com>
2025-05-26 14:26:13 +0200

    Add state-resetting cpgs

--- modular_robot/revolve2/modular_robot/brain/cpg/__init__.py
+++ modular_robot/revolve2/modular_robot/brain/cpg/__init__.py
@@ -2,10 +2,13 @@
 
 from ._brain_cpg_instance import BrainCpgInstance
 from ._brain_cpg_instance_locomotion import BrainCpgInstanceLocomotion
+from ._brain_cpg_instance_locomotion_newstate import BrainCpgInstanceLocomotionNewstate
 from ._brain_cpg_network_neighbor import BrainCpgNetworkNeighbor
 from ._brain_cpg_network_neighbor_random import BrainCpgNetworkNeighborRandom
 from ._brain_cpg_network_static import BrainCpgNetworkStatic
 from ._brain_cpg_network_locomotion import BrainCpgNetworkLocomotion
+from ._brain_cpg_network_locomotion_newstate import BrainCpgNetworkLocomotionNewstate
+
 from ._cpg_network_structure import CpgNetworkStructure
 from ._make_cpg_network_structure_neighbor import (
     active_hinges_to_cpg_network_structure_neighbor,
@@ -13,11 +16,13 @@ from ._make_cpg_network_structure_neighbor import (
 
 __all__ = [
     "BrainCpgInstance",
-    "BrainCpgInstanceLocomotion"
+    "BrainCpgInstanceLocomotion",
+    "BrainCpgInstanceLocomotionNewstate",
     "BrainCpgNetworkNeighbor",
     "BrainCpgNetworkNeighborRandom",
     "BrainCpgNetworkStatic",
     "BrainCpgNetworkLocomotion",
+    "BrainCpgNetworkLocomotionNewstate",
     "CpgNetworkStructure",
     "active_hinges_to_cpg_network_structure_neighbor",
 ]
